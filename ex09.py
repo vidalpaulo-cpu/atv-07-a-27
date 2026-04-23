75737f363eebec0f3f00c38c7b1016d3956d71b8
vidalpaulo-cpu <vidal.paulo@escola.pr.gov.br>
2026-04-23 12:49:23 -0300

    Update ex09.py

---
+++ ex09.py
@@ -0,0 +1,10 @@
+# Vericando se um número é positivo ou negativo
+numero = int(input(digite um número"))
+
+#criando uma condição
+if numero > 0:
+      print("número positivo)
+elif numero == 0:
+        print("número é zero")
+else:
+          print ("número negativo")

611b0f01e51571d524a49d9acbceddb1a88d744a
vidalpaulo-cpu <vidal.paulo@escola.pr.gov.br>
2026-04-24 09:09:46 -0300

    Update ex12.py

---
+++ ex12.py
@@ -0,0 +1,9 @@
+# Exercícios 12 - número par ímpar
+número = int(input("digite um número"))
+
+if numero > 0:
+    print("número positivo")
+elif numero == 0:
+       print("Número é zero")
+else:
+   print("Número negativo")

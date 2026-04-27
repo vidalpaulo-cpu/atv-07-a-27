0059f9cfeb61096e31222813ae016085d9a2bdcc
vidalpaulo-cpu <vidal.paulo@escola.pr.gov.br>
2026-04-27 10:51:41 -0300

    Update ex13.py

---
+++ ex13.py
@@ -0,0 +1,10 @@
+#ex13 - sisteminha de nota 
+
+nota = float(input("digite a nota : "))
+
+if nota >= 18:
+   print("Aprovadooooo!")
+elif nota >= 8 and nota < 17 :
+  print("Ficou na rcp")
+else:
+  print("Reprovouuu")

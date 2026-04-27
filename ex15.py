8dfa06fe70d981b228a9a6b8d32421bc6f4edb78
vidalpaulo-cpu <vidal.paulo@escola.pr.gov.br>
2026-04-27 11:01:45 -0300

    Update ex15.py

---
+++ ex15.py
@@ -0,0 +1,9 @@
+#15 - Sistema de desconto 
+valor = float(input("Valorda compra: "))
+
+if valor >= 200:
+  print("Desconto desconto de 20%")
+elif valor >= 100:
+  print("Descinto de 10%")
+else:
+  print("Sem desconto")

24d6ee66ac3589850884433449aa72edbdf0873a
vidalpaulo-cpu <vidal.paulo@escola.pr.gov.br>
2026-04-27 10:57:00 -0300

    Update ex14.py

---
+++ ex14.py
@@ -0,0 +1,15 @@
+# ex 14 - faixa etária
+
+idade = int(input("Digite a idade:"))
+
+if idade < 12: 
+   print("Criança")
+
+elif idade <18:
+  print("adolescente")
+
+elif idade <60:
+   print(Adulto")
+
+else:
+   prin("Idoso")

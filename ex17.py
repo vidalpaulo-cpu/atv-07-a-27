ad415007921dee69532ffd141df80018a2e9984f
vidalpaulo-cpu <vidal.paulo@escola.pr.gov.br>
2026-04-28 11:52:09 -0300

    Update ex17.py

---
+++ ex17.py
@@ -0,0 +1,8 @@
+#ex 17 - Login simples
+usuario = input("usuário:")
+senha = input("Sistemas:")
+
+if usuario == "admin" and senha =="123":
+   print("login realizado")
+else:
+  print("Usuário ou senha incorretos")

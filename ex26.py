e4499eea69ccd66977f7c903b3a85b98b1ddaa6b
vidalpaulo-cpu <vidal.paulo@escola.pr.gov.br>
2026-04-29 19:42:54 -0300

    Update ex26.py

---
+++ ex26.py
@@ -0,0 +1,21 @@
+# Exercicio 26 - Menu simples de pedidos
+
+print("1 - Hambúrguer - R$ 15.00")
+print("2- Pizza - R$ 20.00")
+print("3- Refrigerante - R$ 8.00")
+
+opcao = int(input("Escolha uma opção:"))
+
+if opcao == 1:
+print("Você escolheu Hambúrguer")
+print("Valor: R$ 15.00")
+elif opcao == 2:
+print("Você escolheu Pizza")
+print("Valor: R$ 20.00")
+elif opcao == 3:
+else:
+print("Você escolheu Refrigerante")
+print("Valor: R$ 8.00")
+print("Opção inválida")
+
+#atividade encerrou aqui por enquanto
